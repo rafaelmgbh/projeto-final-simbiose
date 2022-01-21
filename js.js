4cd7e590ecb0c2c195ea1535d743d29214ddf87e
Rafael Santos de Araujo <rafael.santos@dev.simbioseventures.com>
2022-01-21 10:34:20 -0300

    api consumo ok , concertado problema no apagar todos que nao apagava os clients

--- js.js
+++ js.js
@@ -1,9 +1,8 @@
 let pessoas = [
-    {
-        nome: '',
-        email: ''
-    }
+    {}
 ];
+pessoas.shift()
+apiConsumo()
 
 let nomeApi = "";
 let emailApi = "";
@@ -25,31 +24,34 @@ function salvar() {
     if (validaEntrada(captura) && itemEditado == 0) {
         arrayItens.push(captura)
         posArray = arrayItens[arrayItens.length - 1]
+
+        apiConsumo()
+
         escreveLista()
         document.getElementById('listid').value = ""
         itemEditado = 0;
-    } else if (validaEntrada(captura)){
+    } else if (validaEntrada(captura)) {
         arrayItens.splice(posItemEdit, 1, captura)
         escreveLista()
         alert("Alteração Feita com Sucesso ! ")
         itemEditado = 0;
         limpaCampo()
     }
-        else {
-                alert("Digite um item válido ")
-        }
+    else {
+        alert("Digite um item válido ")
     }
-   
-function detectaEnter (){
-    $('#listid').keydown(function(e){
-        if (e.which == 13){
-          salvar()
+}
+
+function detectaEnter() {
+    $('#listid').keydown(function (e) {
+        if (e.which == 13) {
+            salvar()
         }
-       })
+    })
 }
 
 
- /* essa condicao esta pq esta recebendo undefined como primeiro parametro  */
+/* essa condicao esta pq esta recebendo undefined como primeiro parametro  */
 function deletar(id) {
     if (id !== undefined) {
         alert("deletando " + id)
@@ -84,96 +86,50 @@ function editar(indice) {
 
 }
 
-/*
+
 function apagarLista() {
     let tabelaId = document.getElementById('areaLista');
     decisao = confirm('Deseja realmente limpar a lista de itens ?')
     if (decisao) {
         arrayItens.splice(0, arrayItens.length)
+        pessoas.splice(0,pessoas.length)
         tabelaId.innerHTML = ""
+        apiConsumo()
     }
 
 }
-
-*/
-
-/*
-    function escreveLista() {
-        let areaLista = document.getElementById('areaLista');
-        let botaoEditar = `<td><i class="fas fa-edit btnedit"></i></td>`
-        let botaoDeletar = `<td><i class="fas fa-trash-alt btndelete" onclick=deletar()></i></td>`
-        fetch('https://randomuser.me/api/?results=1')
-        .then((resp) => resp.json())
-        .then(function (data) {
-            let authors = data.results;
-            return authors.map(function (author) {
-                   
-               nomeApi = (author.name.first);
-
-                emailApi = (author.email)
-            })
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-  
-            
-            $("#areaLista").append(`<tr>
-                <td class="idLista"> ${contadorId}</td>
-                <td >
-                ${nomeApi}
-                <br>
-                ${emailApi})
-                </td>
-                
-                <td id=id${contadorId}>${captura}</td>
-                <td onclick="editar(${contadorId}">
-                    <i class="fas fa-edit btnedit" aria-hidden="true"> </i>
-                    </td> 
-                    <td onclick="deletar(${contadorId}">
-                    <i class="fas fa-trash-alt btndelete" onclick="deletar()"> </i>
-                    </td>
-                </tr>` );
-                contadorId ++;
-               bbb = document.querySelector('#id3').value 
-
-*/
 
 function escreveLista() {
     let areaLista = document.getElementById('areaLista');
     let botaoEditar = `<td><i class="fas fa-edit btnedit"></i></td>`
     let botaoDeletar = `<td><i class="fas fa-trash-alt btndelete" onclick=deletar()></i></td>`
     areaLista.innerHTML = ""
+
     for (let i = 0; i < arrayItens.length; i++) {
         let tr = areaLista.insertRow();
-
         let td_id = tr.insertCell();
         td_id.classList.add("idLista")
         td_id.innerText = i
         let td_foto = tr.insertCell();
         td_foto.id = (`id${i}`)
-
-        /* Aqui e feito a impressao do conteudo da API */
-        td_foto.innerHTML = (`${pessoas[i].nomeApi} <br> E:${pessoas[i].emailAp}`)
-        /*  td_foto.innerText = cccc */
+        td_foto.innerHTML = (`${pessoas[i].nomeAp} <br> E: ${pessoas[i].emailAp}`)
         let td_nome = tr.insertCell();
         td_nome.innerHTML = arrayItens[i]
         let td_editar = tr.insertCell();
         td_editar.setAttribute("onclick", "editar(" + i + ")")
         td_editar.innerHTML = botaoEditar
-        let td_deletar = tr.insertCell();     
+        let td_deletar = tr.insertCell();
         td_deletar.setAttribute("onclick", "deletar(" + i + ")")
         td_deletar.innerHTML = botaoDeletar;
         td_id.classList.add('idLista')
-        apiConsumo()
+
     }
+
 }
 
 /* A validacao para vazio esta funcionando porem a validacao para numero nao esta */
 function validaEntrada(entrada) {
-    if (entrada == "" ) {
-       
-        
+    if (entrada == "") {
         return false
         /* alert("Informe um Item \n ( Espaço vazio ou numeros nao são aceitos )") */
     } else return true
@@ -187,11 +143,8 @@ function apiConsumo() {
             let authors = data.results;
             return authors.map(function (author) {
                 let nomeAp = (author.name.first);
-                let emailAp = (author.email)
-                /* console.log('teste da api: ' + nomeApi)*/
-                nomeApi = nomeAp;
-                emailAp = emailAp;
-                pessoas.push({ emailAp, nomeApi });
+                let emailAp = (author.email)           
+                pessoas.push({ emailAp, nomeAp });
                 console.log(pessoas)
             })
         })
@@ -200,7 +153,6 @@ function apiConsumo() {
         });
 }
 
-
-function limpaCampo(){
+function limpaCampo() {
     document.getElementById("listid").value = ""
 }
